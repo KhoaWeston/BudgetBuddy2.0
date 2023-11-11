f62dfb649d6db8ca8678356b1fd1f671a2ed9f8e
Francesca Queary <fhqueary21@my.trine.edu>
2023-11-11 12:10:14 -0500

    Updated Home to work with Login

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,33 @@
-import React from "react";
+import { Button } from '@mui/material'
+import { useContext } from 'react';
+import { UserContext } from '../contexts/user.context';
 import Header from './Header.js';
 import Footer from './Footer.js';
-
-export const Home=(props)=>{
-    return(
-        <div className="home">
-            {/* <Header />
-            <Footer /> */}
-            <h1>This is the home page</h1>
-        </div>
-    )
-}
-export default Home
+ 
+export default function Home() {
+ const { logOutUser } = useContext(UserContext);
+ 
+ // This function is called when the user clicks the "Logout" button.
+ const logOut = async () => {
+   try {
+     // Calling the logOutUser function from the user context.
+     const loggedOut = await logOutUser();
+     // Now we will refresh the page, and the user will be logged out and
+     // redirected to the login page because of the <PrivateRoute /> component.
+     if (loggedOut) {
+       window.location.reload(true);
+     }
+   } catch (error) {
+     alert(error)
+   }
+ }
+ 
+ return (
+   <>
+    <Header/>
+    <Footer/>
+     <h1>Welcome to BudgetBuddy</h1>
+     <Button variant="contained" onClick={logOut}>Logout</Button>
+   </>
+ )
+};
