12b1b480e65a2513332d860bcd736c90042d66cf
KhoaWeston <khoaweston@gmail.com>
2023-11-14 10:58:17 -0500

    Changed Progress page

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,9 +1,29 @@
-import React from "react";
+import { Button } from '@mui/material'
+import { useContext } from 'react';
+import { UserContext } from '../contexts/user.context';
 
 const Profile=()=>{
+    const { logOutUser } = useContext(UserContext);
+    // This function is called when the user clicks the "Logout" button.
+    const logOut = async () => {
+        try {
+            // Calling the logOutUser function from the user context.
+            const loggedOut = await logOutUser();
+            // Now we will refresh the page, and the user will be logged out and
+            // redirected to the login page because of the <PrivateRoute /> component.
+            if (loggedOut) {
+            window.location.reload(true);
+            }
+        } catch (error) {
+            alert(error)
+        }
+    }
     return(
-        <div>
-            <h1>Display profile here</h1>
+        <div className="container">
+            <div>email: </div>
+            <div>password: </div>
+            <button>Edit Profile</button>
+            <Button variant="contained" onClick={logOut}>Logout</Button>
         </div>
     )
 }
--- client/src/components/Progress.js
+++ client/src/components/Progress.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Progress=()=>{
+    const [progress, setProgress] = useState(0);
+    const handlebuttonClick = ()=>{
+        if (progress < 100){
+            setProgress(progress + 20);
+        }
+    }
+    const handlebuttonReset=()=>{
+        setProgress(0);
+    }
+    const getColor=()=>{
+        if(progress < 40){
+            return "#ff0000";
+        } else if (progress < 70){
+            return "#ffa500";
+        } else {
+            return "#2ecc71";
+        }
+    }
+
     return(
-        <div>
-            <h1>Display progress here</h1>
+        <div className="container">
+            <div className="progress-bar">
+                <div className="progress-bar-fill" style={{ width: `${progress}%`, backgroundColor: getColor() }}></div>
+            </div>
+            <div className="progress-label">{progress}</div>
+            <button onClick={handlebuttonClick}>Progress</button>
+            <button onClick={handlebuttonReset}>Reset</button>
         </div>
     )
 }
