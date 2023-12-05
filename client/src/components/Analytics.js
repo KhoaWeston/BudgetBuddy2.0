02c70b1bf3683dab68c7b824af22f1c045ccde85
Francesca Queary <fhqueary21@my.trine.edu>
2023-12-05 13:19:54 -0500

    Fixed chart to only display user's info

--- client/src/components/Analytics.js
+++ client/src/components/Analytics.js
@@ -1,38 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import Header from './Header.js';
 import Footer from './Footer.js';
 import { Button } from '@mui/material';
 import { App } from "realm-web";
 import { APP_ID } from "../contexts/realm/constants.js";
 import ChartsEmbedSDK, { getRealmUserToken } from '@mongodb-js/charts-embed-dom';
-//import { UserContext } from "../contexts/user.context";
-//import { ObjectId } from 'mongodb';
+import { UserContext } from "../contexts/user.context";
 
 const app = new App(APP_ID); // Creating a Realm App Instance
 
-// Embeds a chart for users that are logged in
-const sdk = new ChartsEmbedSDK( {
-    baseUrl: "https://charts.mongodb.com/charts-project-0-qxtjo",
-    autoRefresh: true,
-    getUserToken: () => getRealmUserToken(app),
-});
-
-// Formats embedded chart 
-const chart1 = sdk.createChart({
-    chartId: '655fc617-5768-4074-84c3-e714d5e34c62',
-    width: "90%",
-    height: 400,
-});
-
-
 const Analytics=()=>{
-    // const { user } = useContext(UserContext);
-    // const userID1 = app.currentUser.id;
-    // const userID2 = user.id;
-    // const userObjectId = new ObjectId('help');
-    // console.log(userID1);
-    // console.log(userID2);
-    // console.log({$toObjectId: "5ab9cbfa31c2ab715d42129e"});
+    const { user } = useContext(UserContext);
+    
+    // Embeds a chart for users that are logged in
+    const sdk = new ChartsEmbedSDK( {
+        baseUrl: "https://charts.mongodb.com/charts-project-0-qxtjo",
+        autoRefresh: true,
+        getUserToken: () => getRealmUserToken(app),
+    });
+
+    // Formats embedded chart 
+    const chart1 = sdk.createChart({
+        chartId: '655fc617-5768-4074-84c3-e714d5e34c62',
+        width: "90%",
+        height: 400,
+        filter: {"author":{'$oid': user.id}} // filters the chart by user
+    });
 
     // Filters chart entries between dates entered by user
     const changeDate = async()=>{
--- client/src/components/inputs/InputGoal.js
+++ client/src/components/inputs/InputGoal.js
@@ -73,9 +73,10 @@ const InputGoal=()=>{
         const goal = await goals.findOne(); // finds one goal
         alert("Your current goal states you want to have $" + goal.amount + " in the category: " + goal.category+
         "\nEnter the category and amount that you want to change your goal to and press Enter Goal"); // alerts the user of their current goa
-        const goal_deleted = await goals.deleteOne(); // deletes their current goal
+         await goals.deleteOne(); // deletes their current goal
       }catch (error){
-        alert("You have no goals at the moment")
+          alert("You have no goals at the moment");
+        
       }
       
     };
